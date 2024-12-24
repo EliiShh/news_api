78f8a74d2ecc52849b3cf6e41f83a8586fac28f0
eli <0555596108c@gmail.com>
2024-12-24 17:52:07 +0200

    Connection to Kafka for sending to elastic

---
+++ app/kafka_publisher/producer.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+
+import os
+import json
+from kafka import KafkaProducer
+
+def send_data(topic:str, data:list):
+    producer = KafkaProducer(
+        bootstrap_servers=os.environ['BOOTSTRAP_SERVERS'],
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+    try:
+        producer.send(topic, value=data)
+        producer.flush()
+        print('Data sent successfully')
+    except Exception as e:
+        print(f'Error sending data: {e}')
+    finally:
+        producer.close()
+
